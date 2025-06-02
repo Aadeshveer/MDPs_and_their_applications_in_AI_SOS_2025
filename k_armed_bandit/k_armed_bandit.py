40cdd90c5e4dc38ab08fd3a63155cc763c430da6
Aadeshveer <24b0926@iitb.ac.in>
2025-06-02 23:20:32 +0800

    Implemented various other algorithms for k_armed_bandits

--- k_armed_bandit/k_armed_bandit.py
+++ k_armed_bandit/k_armed_bandit.py
@@ -3,15 +3,21 @@ import numpy as np
 
 class KArmedBandit:
 
-    def __init__(self, k=10, std_dev=1, means=None):
+    def __init__(self, k=10, stationary=True, std_dev=1, means=None):
         self.k = k
         self.std_dev = std_dev
+        self.stationary = stationary
         if means is None:
-            self.means = 2 * np.random.random(size=k) - 1
+            if self.stationary:
+                self.means = 2 * np.random.random(size=k) - 1
+            else:
+                self.means = np.zeros(k)
         else:
             self.means = means
 
     def draw(self, i):
+        if not self.stationary:
+            self.means += np.random.normal(loc=0, scale=0.01, size=self.k)
         return np.random.normal(loc=self.means[i], scale=self.std_dev)
 
     def get_optimal(self):
@@ -20,19 +26,45 @@ class KArmedBandit:
 
 class EpsilonGreedy:
 
-    def __init__(self, epsilon=0.1, k=10):
+    def __init__(
+        self,
+        epsilon=0.1,
+        k=10,
+        stationary=True,
+        alpha=0.1,
+        Q_initialization=0,
+        baseline=0
+    ):
         self.epsilon = epsilon
         self.k = k
-        self.Q = np.zeros(k)
+        self.stationary = stationary
+        self.alpha = alpha
+        self.Q = np.ones(k) * Q_initialization
         self.N = np.zeros(k)
+        self.H = np.zeros(k)
+        self.pi = np.ones(k) / k
+        self.R_mean = baseline
 
-    def choose_action(self):
+    def choose_action(self, UCB=False, Gradient=False, c=2):
+        if UCB:
+            return np.argmax(
+                self.Q + c * np.sqrt(np.log(self.N.sum()) / self.N)
+            )
+        if Gradient:
+            return np.random.choice(np.arange(self.k), p=self.pi)
         if np.random.random() < self.epsilon:
             prediction = np.random.randint(0, self.k)
         else:
             prediction = np.argmax(self.Q)
         return prediction
 
-    def update_table(self, i, reward):
+    def update_table(self, i, reward, grad=False):
         self.N[i] += 1
-        self.Q[i] = self.Q[i] + (reward - self.Q[i]) / self.N[i]
+        if grad:
+            self.H = self.H - self.alpha * (reward - self.R_mean) * (self.pi)
+            self.H[i] += self.alpha * (reward - self.R_mean)
+            self.pi = np.exp(self.H) / np.sum(np.exp(self.H))
+            self.R_mean = self.R_mean + (reward - self.R_mean) / self.N.sum()
+        else:
+            alpha_factor = (1 / self.N[i] if self.stationary else self.alpha)
+            self.Q[i] = self.Q[i] + (reward - self.Q[i]) * alpha_factor
--- k_armed_bandit/main.py
+++ k_armed_bandit/main.py
@@ -2,75 +2,322 @@ import numpy as np
 import matplotlib.pyplot as plt
 from k_armed_bandit import KArmedBandit, EpsilonGreedy
 
-epsilon_values = [0, 0.01, 0.1]
-total_iterations = 1000
-runs_per_iteration = 2000
+total_iterations = 10000
+runs_per_iteration = 1000
 bandit_arms = 10
 
-results_arr = np.zeros(
-    (
-        len(epsilon_values),
-        runs_per_iteration,
-        total_iterations,
-    )
-)
 
-optimal_arr = np.zeros(
-    (
-        len(epsilon_values),
-        runs_per_iteration,
-        total_iterations,
-    )
-)
+def run_tests(
+    machine_function,
+    agent_function,
+    total_itr=total_iterations,
+    rpi=runs_per_iteration,
+    UCB=False,
+    gradient=False,
+):
+    # first array stores the reward and second stores optimal moves
+    results_arr = np.zeros((2, rpi, total_itr))
 
-for i_eps, epsilon in enumerate(epsilon_values):
-
-    print(f'Running tests for epsion {epsilon} ...')
-
-    machine = KArmedBandit(k=bandit_arms)
-
-    for i in range(total_iterations):
+    for i in range(total_itr):
 
         if i % 200 == 0:
-            print(f'Running iteration {i}/{total_iterations}')
+            print(f'Running iteration {i}/{total_itr}')
 
-        agent = EpsilonGreedy(epsilon=epsilon, k=bandit_arms)
+        machine = machine_function()
+        agent = agent_function()
 
         optimum_act = machine.get_optimal()
 
-        for j in range(runs_per_iteration):
+        for j in range(rpi):
 
-            action = agent.choose_action()
+            action = agent.choose_action(UCB, gradient)
             result = machine.draw(action)
-            agent.update_table(action, reward=result)
+            agent.update_table(action, reward=result, grad=gradient)
 
-            results_arr[i_eps, j, i] = result
+            results_arr[0, j, i] = result
 
             if action == optimum_act:
-                optimal_arr[i_eps, j, i] = 1
+                results_arr[1, j, i] = 1
 
-avg_result = results_arr.mean(axis=2)
-optimal_result = optimal_arr.mean(axis=2)
+    return results_arr.mean(axis=2)
 
-plt.subplot(2, 1, 1)
 
-for i, epsilon in enumerate(epsilon_values):
-    plt.plot(avg_result[i], label=f'Epsilon : {epsilon}')
+def machine_function_generator(stationary=True, bandit_arms=bandit_arms):
+    return lambda: KArmedBandit(k=bandit_arms, stationary=stationary)
 
-plt.title('Average performance of Epsilong greedy algorithm with various k')
-plt.xlabel('Steps')
-plt.ylabel('Average Reward')
-plt.legend()
 
-plt.subplot(2, 1, 2)
+def agent_function_generator(
+    eps=0.1,
+    stationary=True,
+    Q_initialization=0,
+    alpha=0.1,
+    baseline=0
+):
+    return lambda: EpsilonGreedy(
+        epsilon=eps,
+        stationary=stationary,
+        Q_initialization=Q_initialization,
+        alpha=alpha,
+        baseline=baseline
+    )
 
-for i, epsilon in enumerate(epsilon_values):
-    plt.plot(optimal_result[i], label=f'Epsilon : {epsilon}')
 
-plt.title('Fraction of optimal performance of Epsilong greedy algorithm with various k')  # Noqa:E501
-plt.xlabel('Steps')
-plt.ylabel('Optimal fraction')
-plt.legend()
+def compare_epsilon(epsilon_values=[0, 0.01, 0.1]):
 
-plt.tight_layout()
-plt.savefig('epsilon_greedy.png')
+    arr = np.zeros((len(epsilon_values), 2, runs_per_iteration))
+
+    for i_eps, epsilon in enumerate(epsilon_values):
+
+        print(f'Running tests for epsion {epsilon} ...')
+
+        arr[i_eps] = run_tests(
+            machine_function_generator(),
+            agent_function_generator(epsilon, True)
+        )
+
+    plt.subplot(2, 1, 1)
+
+    for i, epsilon in enumerate(epsilon_values):
+        plt.plot(arr[i][0], label=f'Epsilon : {epsilon}')
+
+    plt.title('Average performance of Epsilong greedy algorithm with various k')  # Noqa:E501
+    plt.xlabel('Steps')
+    plt.ylabel('Average Reward')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+
+    for i, epsilon in enumerate(epsilon_values):
+        plt.plot(arr[i][1], label=f'Epsilon : {epsilon}')
+
+    plt.title('Fraction of optimal performance of Epsilong greedy algorithm with various k')  # Noqa:E501
+    plt.xlabel('Steps')
+    plt.ylabel('Optimal fraction')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('epsilon_greedy.png')
+
+
+def compare_stationary():
+    arr = np.zeros((3, 2, runs_per_iteration))
+
+    print('Running tests for Stationary machine ...')
+
+    arr[0] = run_tests(
+        machine_function_generator(True),
+        agent_function_generator(0.1, True)
+    )
+
+    print(
+        'Running tests for non stationary machine and constant step size ...'
+    )
+
+    arr[1] = run_tests(
+        machine_function_generator(False),
+        agent_function_generator(0.1, False)
+    )
+
+    print('Running tests for non stationary machine and sample-averaging ...')
+
+    arr[2] = run_tests(
+        machine_function_generator(False),
+        agent_function_generator(0.1, True)
+    )
+
+    plt.figure(figsize=(8, 8))
+
+    plt.subplot(2, 1, 1)
+
+    plt.plot(arr[0, 0], label='Stationary')
+    plt.plot(arr[1, 0], label='Non Stationary with constant step size parameter')  # Noqa:E501
+    plt.plot(arr[2, 0], label='Non Stationary with 1/n step size parameter')
+
+    plt.title('Average performance of Epsilong greedy algorithm with non stationary input')  # Noqa:E501
+    plt.xlabel('Steps')
+    plt.ylabel('Average Reward')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+
+    plt.plot(arr[0, 1], label='Stationary')
+    plt.plot(arr[1, 1], label='Non Stationary with constant step size parameter')  # Noqa:E501
+    plt.plot(arr[2, 1], label='Non Stationary with sample averaging')  # Noqa:E501
+
+    plt.title('Fraction of optimal performance of Epsilong greedy algorithm with non stationary input')  # Noqa:E501
+    plt.xlabel('Steps')
+    plt.ylabel('Optimal fraction')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('epsilon_greedy_non_stationary.png')
+
+
+def optimistic_greedy():
+    arr = np.zeros((2, 2, runs_per_iteration))
+
+    print('Running tests for Epsilon greedy agent ...')
+
+    arr[0] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(0.1)
+    )
+
+    print('Running tests for Optimistic greedy agent ...')
+
+    arr[1] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(0, Q_initialization=5)
+    )
+
+    plt.figure(figsize=(8, 8))
+
+    plt.subplot(2, 1, 1)
+
+    plt.plot(arr[0, 0], label='Non Optimistic')
+    plt.plot(arr[1, 0], label='Optimistic greedy')
+
+    plt.title('Average performance')
+    plt.xlabel('Steps')
+    plt.ylabel('Average Reward')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+
+    plt.plot(arr[0, 1], label='Non Optimistic')
+    plt.plot(arr[1, 1], label='Optimistic greedy')
+
+    plt.title('Fraction of optimal performance')
+    plt.xlabel('Steps')
+    plt.ylabel('Optimal fraction')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('optimistic_epsilon_greedy.png')
+
+
+def upper_confidence_bound():
+
+    arr = np.zeros((2, 2, runs_per_iteration))
+
+    print('Running tests for Epsilon greedy agent ...')
+
+    arr[0] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(0.1),
+    )
+
+    print('Running tests for UCB agent ...')
+
+    arr[1] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(0),
+        UCB=True,
+    )
+
+    plt.figure(figsize=(8, 8))
+
+    plt.subplot(2, 1, 1)
+
+    plt.plot(arr[0, 0], label='Epsilon greedy (0.1)')
+    plt.plot(arr[1, 0], label='UCB (c=2)')
+
+    plt.title('Average performance')
+    plt.xlabel('Steps')
+    plt.ylabel('Average Reward')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+
+    plt.plot(arr[0, 1], label='Epsilon greedy (0.1)')
+    plt.plot(arr[1, 1], label='UCB (c=2)')
+
+    plt.title('Fraction of optimal performance')
+    plt.xlabel('Steps')
+    plt.ylabel('Optimal fraction')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('UCB.png')
+
+
+def gradient_bandit_algo():
+
+    arr = np.zeros((5, 2, runs_per_iteration))
+
+    print('Running tests for Epsilon greedy agent ...')
+
+    arr[0] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(0.1),
+    )
+
+    print('Running tests for gradient(0.1 baseless) agent ...')
+
+    arr[1] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(alpha=0.1),
+        gradient=True,
+    )
+
+    print('Running tests for gradient(0.4 baseless) agent ...')
+
+    arr[2] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(alpha=0.4),
+        gradient=True,
+    )
+
+    print('Running tests for gradient(0.1 base) agent ...')
+
+    arr[3] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(alpha=0.1, baseline=4),
+        gradient=True,
+    )
+
+    print('Running tests for gradient(0.4 base) agent ...')
+
+    arr[4] = run_tests(
+        machine_function_generator(),
+        agent_function_generator(alpha=0.4, baseline=4),
+        gradient=True,
+    )
+
+    plt.figure(figsize=(8, 8))
+
+    plt.subplot(2, 1, 1)
+
+    plt.plot(arr[0, 0], label='Epsilon greedy (0.1)')
+    plt.plot(arr[1, 0], label='alpha 0.1, baseless')
+    plt.plot(arr[2, 0], label='alpha 0.4, baseless')
+    plt.plot(arr[3, 0], label='alpha 0.1, with base')
+    plt.plot(arr[4, 0], label='alpha 0.4, with base')
+
+    plt.title('Average performance')
+    plt.xlabel('Steps')
+    plt.ylabel('Average Reward')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+
+    plt.plot(arr[0, 1], label='Epsilon greedy (0.1)')
+    plt.plot(arr[1, 1], label='alpha 0.1, baseless')
+    plt.plot(arr[2, 1], label='alpha 0.4, baseless')
+    plt.plot(arr[3, 1], label='alpha 0.1, with base')
+    plt.plot(arr[4, 1], label='alpha 0.4, with base')
+
+    plt.title('Fraction of optimal performance')
+    plt.xlabel('Steps')
+    plt.ylabel('Optimal fraction')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('gradient.png')
+
+
+compare_epsilon()
+compare_stationary()
+optimistic_greedy()
+upper_confidence_bound()
+gradient_bandit_algo()
