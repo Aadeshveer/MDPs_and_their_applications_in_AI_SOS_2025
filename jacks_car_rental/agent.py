b00cdabe467977f13e29526685acdf16b4cbbad8
Aadeshveer <aadeshveer07@gmail.com>
2025-06-13 08:45:24 +0530

    Used numpy for faster Jack's rental problem solution, getting proper results

--- jacks_car_rental/agent.py
+++ jacks_car_rental/agent.py
@@ -1,42 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns  # for generating beautiful heatmaps
 
 
 class Agent:
 
     GAMMA = 0.9
-    RENT = 10
-    SHIFT_COST = 2
     MAX_CARS = 20
     MAX_MOVE = 5
-    LOSE_PENALTY = -1000
-    DELTA = 0.1
+    DELTA = 0.01
 
     def __init__(self, action_function):
-        self.num_states = 2
         self.action_function = action_function
         self.initialize_states()
 
     def initialize_states(self):
 
-        self.values = np.zeros((self.MAX_CARS+1) ** 2 + 1)
-        self.pi = np.ones(((self.MAX_CARS+1)**2 + 1, 2*self.MAX_MOVE+1))
-        self.pi = self.pi / self.pi.shape[1]
+        self.values = np.zeros(
+            (
+                self.MAX_CARS+1,
+                self.MAX_CARS+1
+            )
+        )
+        self.pi = np.ones(
+            (
+                self.MAX_CARS+1,
+                self.MAX_CARS+1,
+                2*self.MAX_MOVE+1
+            )
+        )
+        self.pi = self.pi / self.pi.shape[2]
 
-        self.state_to_idx = {}
-        self.idx_to_state = {}
-
-        ctr = 0
-
-        for i in range(self.MAX_CARS + 1):
-
-            for j in range(self.MAX_CARS + 1):
-
-                ctr += 1
-                self.state_to_idx[(i, j)] = ctr
-                self.idx_to_state[ctr] = (i, j)
-
-    def evaluate_policy(self):
+    def evaluate_policy(self, synchronous=False):
 
         diff = np.inf
 
@@ -44,86 +39,145 @@ class Agent:
 
             diff = 0
 
+            temp_values = self.values.copy() if synchronous else self.values
+
             # for all states
-            for i in self.idx_to_state:
-                if i % 5 == 0:
-                    print(f'Evaluating policy for : {i}/{len(self.idx_to_state)}, difference is {diff}')
-                v = 0
+            for i in range(temp_values.shape[0]):
+                print(f'Evaluating policy for : {i}/{temp_values.shape[0]}, difference is {diff}')  # Noqa:E501
+                for j in range(temp_values.shape[1]):
+                    v = 0
 
-                # for all actions
-                for k in range(self.pi.shape[1]):
+                    # for all actions
+                    for k in range(self.pi.shape[2]):
 
-                    probability = self.pi[i, k]
+                        probability = self.pi[i, j, k]
 
-                    result = self.action_function(
-                        k-self.MAX_MOVE,
-                        self.idx_to_state[i],
-                        self.values,
-                        self.GAMMA,
-                        self.state_to_idx
-                    )
-
-                    v += probability * result
-
-                diff = max(diff, abs(v-self.values[i]))
-                self.values[i] = v
+                        result = self.action_function(
+                            k-self.MAX_MOVE,
+                            (i, j),
+                            temp_values,
+                            self.GAMMA,
+                        )
+                        v += probability * result
+                    diff = max(diff, abs(v-temp_values[i, j]))
+                    self.values[i, j] = v
 
     def improve_policy(self):
 
         # for all states
-        for i in self.idx_to_state:
+        for i in range(self.values.shape[0]):
+            print(f'Improving policy for : {i}/{self.values.shape[0]}')
+            for j in range(self.values.shape[1]):
 
-            if i % 20 == 0:
-                print(f'Improving policy for state {i}/{len(self.idx_to_state)}')
+                max_ind = [self.pi[i, j].argmax()]
+                max_val = -np.inf
 
-            max_ind = [self.pi[i].argmax()]
-            max_val = -np.inf
+                for k in range(self.pi.shape[2]):
+                    if k-self.MAX_MOVE > i or -k+self.MAX_MOVE > j:
+                        continue
+                    result = self.action_function(
+                        k-self.MAX_MOVE,
+                        (i, j),
+                        self.values,
+                        self.GAMMA,
+                    )
 
-            for k in range(self.pi.shape[1]):
+                    new_val = result
 
-                result = self.action_function(
-                    k-self.MAX_MOVE,
-                    self.idx_to_state[i],
-                    self.values,
-                    self.GAMMA,
-                    self.state_to_idx
-                )
+                    if (new_val > max_val):
+                        max_val = new_val
+                        max_ind = [k]
+                    elif (new_val == max_val):
+                        max_ind.append(k)
+                self.pi[i, j, ...] = 0
+                for k in max_ind:
+                    self.pi[i, j, k] = 1 / len(max_ind)
 
-                new_val = result
+    def iterate_policy(self, synchronous=False, save=None):
+        old_policy = self.pi.copy()
+        ctr = 1
+        self.evaluate_policy(synchronous)
+        self.improve_policy()
+        if synchronous:
+            self.show_policy(f'results/policy_after_iteration{ctr}.png')
+        while (old_policy != self.pi).any():
+            ctr += 1
+            old_policy = self.pi.copy()
+            self.evaluate_policy(synchronous)
+            self.improve_policy()
+            if synchronous:
+                self.show_policy(f'results/policy_after_iteration{ctr}.png')
 
-                if (new_val > max_val):
-                    max_val = new_val
-                    max_ind = [k]
-                elif (new_val == max_val):
-                    max_ind.append(k)
-            self.pi[i, ...] = 0
-            for k in max_ind:
-                self.pi[i, k] = 1 / len(max_ind)
+    def iteratate_values(self):
 
-    def show_values(self, save=None, slip=0):
+        diff = np.inf
 
-        grid = np.zeros((self.MAX_CARS+1, self.MAX_CARS+1))
-        for state in self.state_to_idx:
-            grid[*state] = self.values[self.state_to_idx[state]]
+        while diff > self.DELTA:
+
+            diff = 0
+
+            temp_values = self.values
+
+            # for all states
+            for i in range(temp_values.shape[0]):
+                print(f'Evaluating policy for : {i}/{temp_values.shape[0]}, difference is {diff}')  # Noqa:E501
+                for j in range(temp_values.shape[1]):
+                    v = 0
+
+                    # for all actions
+                    for k in range(self.pi.shape[2]):
+
+                        result = self.action_function(
+                            k-self.MAX_MOVE,
+                            (i, j),
+                            temp_values,
+                            self.GAMMA,
+                        )
+                        v = max(result, v)
+                    diff = max(diff, abs(v-temp_values[i, j]))
+                    self.values[i, j] = v
+
+    def show_values(self, save=None):
 
         plt.figure(figsize=(8, 8))
-        plt.imshow(grid, cmap='inferno')
-        plt.colorbar()
-        plt.xticks(np.arange(self.MAX_CARS+1))
-        plt.xlabel('Cars at location 1')
-        plt.yticks(np.arange(self.MAX_CARS+1))
-        plt.ylabel('Cars at location 2')
-        for i in range(grid.shape[0]):
-            for j in range(grid.shape[1]):
-                plt.text(
-                    i,
-                    j,
-                    '{:.2f}'.format(grid[i, j]),
-                    ha='center',
-                    va='center',
-                    fontsize=6,
-                    color='white',
+        plot_object = sns.heatmap(
+            np.rot90(self.values),
+            square=True,
+            yticklabels=np.arange(self.MAX_CARS, -1, -1),
+            xticklabels=np.arange(self.MAX_CARS + 1),
+        )
+        plot_object.set(
+            xlabel='Cars at location 2',
+            ylabel='Cars at location 1',
+            title='Values'
+        )
+        if save is None:
+            plt.show()
+        else:
+            plt.savefig(save)
+        plt.close()
+
+    def show_policy(self, save=None):
+        grid = np.zeros((self.MAX_CARS+1, self.MAX_CARS+1))
+        for i in range(self.MAX_CARS+1):
+            for j in range(self.MAX_CARS+1):
+                grid[i, j] = np.average(
+                    np.arange(-self.MAX_MOVE, self.MAX_MOVE+1),
+                    weights=self.pi[i, j]
                 )
+        grid = np.rot90(grid.T)
+        plt.figure(figsize=(8, 8))
+        plot_object = sns.heatmap(
+            grid,
+            square=True,
+            yticklabels=np.arange(self.MAX_CARS, -1, -1),
+            xticklabels=np.arange(self.MAX_CARS + 1),
+        )
+        plot_object.set(
+            xlabel='Cars at location 2',
+            ylabel='Cars at location 1',
+            title='Policy'
+        )
         if save is None:
             plt.show()
         else:
--- jacks_car_rental/problem.py
+++ jacks_car_rental/problem.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy  # for poisson probability
 from agent import Agent
 
 
@@ -8,103 +9,102 @@ class Env:
     SHIFT_COST = 2
     MAX_CARS = 20
     MAX_MOVE = 5
-    LOSE_PENALTY = -10
-    SAMPLE_SIZE = 100
-    MAX_ITERATION = 10
+    MAX_ITERATION = 20
+    PARKING_COST = 4
+    PARKING_LIMIT = 10
 
-    def __init__(self):
+    def __init__(self, updated_test=False):
         self.out_lambda = np.array([3, 4])
         self.in_lambda = np.array([2, 3])
         self.next_returns = np.zeros(2)
         self.agent = Agent(self.simulate_day)
+        self.updated_test = updated_test
 
-    def simulate_day(self, movement, cars, values, gamma, state_to_index):
+    def simulate_day(self, movement, cars, values, gamma):
 
-        prob = 1
         result = 0
-        ncars = list(cars)
+        shape = (
+            self.MAX_ITERATION,
+            self.MAX_ITERATION,
+            self.MAX_ITERATION,
+            self.MAX_ITERATION,
+        )
+        ncars = np.zeros((
+            2,
+            *shape
+        ))
+        ncars[0] = cars[0]
+        ncars[1] = cars[1]
+        credits = np.zeros(shape)
 
         # all possible next states
-        for request_1 in range(self.MAX_ITERATION):
-            prob1 = prob * self.poisson_probability(
-                request_1, self.out_lambda[0]
-            )
+        prob1 = scipy.stats.poisson.pmf(
+            np.arange(self.MAX_ITERATION), self.out_lambda[0]
+        ).reshape((-1, 1, 1, 1))
+        prob2 = scipy.stats.poisson.pmf(
+            np.arange(self.MAX_ITERATION), self.out_lambda[1]
+        ).reshape((1, -1, 1, 1))
+        prob3 = scipy.stats.poisson.pmf(
+            np.arange(self.MAX_ITERATION), self.in_lambda[0]
+        ).reshape((1, 1, -1, 1))
+        prob4 = scipy.stats.poisson.pmf(
+            np.arange(self.MAX_ITERATION), self.in_lambda[1]
+        ).reshape((1, 1, 1, -1))
 
-            for request_2 in range(self.MAX_ITERATION):
-                prob2 = prob1 * self.poisson_probability(
-                    request_2,
-                    self.out_lambda[1]
-                )
+        ncars[0] -= movement
+        ncars[1] += movement
+        credits -= self.SHIFT_COST * abs(movement)
 
-                for returns_1 in range(self.MAX_ITERATION):
-                    prob3 = prob2 * self.poisson_probability(
-                        returns_1,
-                        self.in_lambda[0]
-                    )
+        # including the special worker case
+        if self.updated_test and movement >= 1:
+            credits += self.SHIFT_COST
 
-                    for returns_2 in range(self.MAX_ITERATION):
-                        prob4 = prob3 * self.poisson_probability(
-                            returns_2,
-                            self.out_lambda[1]
-                        )
+        ncars[0] = np.clip(ncars[0], 0, self.MAX_CARS)
+        ncars[1] = np.clip(ncars[1], 0, self.MAX_CARS)
 
-                        cars = ncars
+        rented_1 = np.minimum(
+            np.arange(self.MAX_ITERATION).reshape(-1, 1, 1, 1),
+            ncars[0]
+        )
+        rented_2 = np.minimum(
+            np.arange(self.MAX_ITERATION).reshape(1, -1, 1, 1),
+            ncars[1]
+        )
+        return_1 = np.arange(self.MAX_ITERATION).reshape(1, 1, -1, 1)
+        return_2 = np.arange(self.MAX_ITERATION).reshape(1, 1, 1, -1)
 
-                        credits = 0
-                        if -self.MAX_MOVE < movement < self.MAX_MOVE:
-                            cars[0] -= movement
-                            cars[1] += movement
-                            if cars[0] < 0 or cars[1] < 0:
-                                cars[0] += movement
-                                cars[1] -= movement
-                            else:
-                                credits -= self.SHIFT_COST * movement
+        ncars[0] = ncars[0] + return_1 - rented_1
+        ncars[1] = ncars[1] + return_2 - rented_2
 
-                        rented_1 = min(request_1, cars[0])
-                        rented_2 = min(request_2, cars[1])
+        # adding parking case
+        if self.updated_test:
+            credits -= (ncars[0] > self.PARKING_LIMIT)*self.PARKING_COST
+            credits -= (ncars[1] > self.PARKING_LIMIT)*self.PARKING_COST
 
-                        cars[0] = cars[0] + returns_1 - rented_1
-                        cars[1] = cars[1] + returns_2 - rented_2
+        credits += (self.RENT*(rented_1+rented_2))
 
-                        credits += self.RENT*(rented_1+rented_2)
+        ncars[0] = np.clip(ncars[0], 0, self.MAX_CARS)
+        ncars[1] = np.clip(ncars[1], 0, self.MAX_CARS)
 
-                        if cars[0] > 20:
-                            cars[0] = 20
-                        elif cars[0] < 0:
-                            credits = self.LOSE_PENALTY
-                            cars[0] = 0
-                        if cars[1] > 20:
-                            cars[1] = 20
-                        elif cars[1] < 0:
-                            credits = self.LOSE_PENALTY
-                            cars[1] = 0
-                        result += prob4 * (credits + gamma * values[
-                            state_to_index[tuple(cars)]
-                        ])
+        ncars = ncars.astype(np.int32)
 
-        return (result)
+        future_value = values[ncars[0], ncars[1]]
 
-    @staticmethod
-    def poisson_probability(k_scalar, lambda_rate):
+        result = prob1*prob2*prob3*prob4 * (credits + gamma * future_value)
 
-        lambda_pow_k = np.power(lambda_rate, k_scalar)
-
-        exp_neg_lambda = np.exp(-lambda_rate)
-
-        if k_scalar == 0:
-            k_factorial = 1.0
-        else:
-            k_factorial = np.prod(np.arange(1, k_scalar + 1, dtype=np.double))
-
-        probability = (lambda_pow_k * exp_neg_lambda) / k_factorial
-
-        return probability
+        return result.sum()
 
 
-world = Env()
-world.agent.show_values()
-for _ in range(20):
-    world.agent.evaluate_policy()
-    world.agent.improve_policy()
-    world.agent.show_values()
-# print(Env.poisson_probability(10, 3))
+# world = Env()
+# world.agent.iterate_policy(True)
+# world.agent.show_values('results/value_function.png')
+
+# world = Env()
+# world.agent.iteratate_values()
+# world.agent.show_values('results/value_iteration.png')
+
+world = Env(True)
+world.agent.iteratate_values()
+world.agent.improve_policy()
+world.agent.show_policy('results/exercise_policy.png')
+world.agent.show_values('results/exercise_values.png')
