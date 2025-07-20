11ee9876f3adf518f23f043dc7cbd32cfa108b03
Aadeshveer <aadeshveer07@gmail.com>
2025-07-20 10:27:22 +0530

    Tried analysing problem with learning failure

--- flappy_bird/game.py
+++ flappy_bird/game.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import sys
 from bird import Bird, BIRD_SIZE, MAX_UP_VEL
-from env import Pipes, Pipe, CLEARANCE, GAME_VEL
+from env import Pipes, Pipe, CLEARANCE
 from utils import load_image, load_images, Animation
 from agent import Agent, State, MemoryPoint
 from plotter import Plotter
@@ -10,6 +10,7 @@ WINDOW_SIZE = (300, 400)
 SCREEN_SIZE = (600, 800)
 SKY_BLUE = (207, 236, 247)
 FPS = 60
+STEP_CONTROL = 4
 
 
 class Game:
@@ -97,15 +98,22 @@ class Game:
     def get_state(self) -> State:
         closest_next_pipe: Pipe = self.pipes.get_next_pipe()
         return State(
-            (self.bird.altitude - closest_next_pipe.height + BIRD_SIZE[1]/2 + CLEARANCE/2)/WINDOW_SIZE[1],  # Noqa:E501
+            (self.bird.altitude - closest_next_pipe.height + CLEARANCE)/WINDOW_SIZE[1],  # Noqa:E501
+            (self.bird.altitude + BIRD_SIZE[1] - closest_next_pipe.height)/WINDOW_SIZE[1],  # Noqa:E501
+            (self.bird.altitude)/WINDOW_SIZE[1],
+            (WINDOW_SIZE[1] - self.bird.altitude + BIRD_SIZE[1])/WINDOW_SIZE[1],  # Noqa:E501
             self.bird.fall_vel/MAX_UP_VEL,
-            closest_next_pipe.x/(GAME_VEL*FPS*2),
-            (self.bird.altitude + BIRD_SIZE[1]/2)/WINDOW_SIZE[1],
+            closest_next_pipe.x/WINDOW_SIZE[0],
         )
 
-    def AI_game(self, interface: bool = False):
+    def AI_game(self, interface: bool = False, log_file: str | None = None):
         score = 0
         n = 0
+        fps = 10000
+        if log_file is not None:
+            with open(log_file, 'w') as f:
+                # print(alert) # IMPORTANT changed w to r
+                f.write('score,episode_length\n')
         if interface:
             self.initialize_pygame()
         print('getting ai')
@@ -113,8 +121,8 @@ class Game:
         print('begining')
         while True:
             n += 1
-            if n % 100 == 0:
-                self.plotter.plot('performance.png')
+            # if n % 100 == 0:
+            #     self.plotter.plot('performance.png')
             done = False
             score = 0
             self.reset()
@@ -136,23 +144,32 @@ class Game:
                         if event.type is pg.QUIT:
                             pg.quit()
                             sys.exit()
+                        if event.type == pg.KEYDOWN:
+                            if event.key == pg.K_SPACE:
+                                if fps == 60:
+                                    fps = 10000
+                                else:
+                                    fps = 60
 
                 ctr += 1
                 done = False
                 state = self.get_state()
-                action = self.agent.choose_action(state)
+                if ctr % STEP_CONTROL == 0:
+                    action = self.agent.choose_action(state)
+                else:
+                    action = False
                 result = self.iterate(action)
                 match result:
                     case -1:
-                        reward = -5
+                        reward = -50
                         done = True
                     case 1:
                         score += result
-                        reward = 10
+                        reward = 1000
                     case 0:
-                        reward = 0
+                        reward = 0.1
                     case -2:
-                        reward = -20
+                        reward = -100
                         done = True
                     case _:
                         raise ValueError(f'result given by iterate method is faulty: {result}')  # Noqa:E501
@@ -163,9 +180,8 @@ class Game:
                     self.get_state(),
                     done
                 )
-                self.agent.train_one_data(memory)
+                # self.agent.train_one_data(memory)
                 self.agent.add_to_memory(memory)
-
                 if interface:
                     self.bird.render()
                     self.pipes.render()
@@ -174,15 +190,21 @@ class Game:
                         pg.transform.scale(self.window, SCREEN_SIZE)
                     )
                     pg.display.update()
-
+                    self.clock.tick(fps)
                 if done:
                     break
+
+            if n % 10 == 0:
+                self.agent.update_target_model()
+                print(f'{n}. score: {score}, iterations: {ctr}, epsilon: {self.agent.epsilon}')
+
             self.agent.train_multiple_data()
-            print(f'{n}. score: {score}, iterations: {ctr}')
-            if ctr >= 1000:
+            if log_file is not None:
+                with open(log_file, 'a') as f:
+                    f.write(f'{score},{ctr}\n')
+            if n % 1000 == 0:
                 self.agent.model.save('model.pth')
-                break
-            self.plotter.add_score(score, ctr)
+            # self.plotter.add_score(score, ctr)
 
     def iterate(self, action: bool) -> int:
         if action:
@@ -198,4 +220,4 @@ class Game:
 
 if __name__ == '__main__':
     game = Game()
-    game.AI_game()
+    game.AI_game(log_file='log1.csv')
--- flappy_bird/model.py
+++ flappy_bird/model.py
@@ -8,12 +8,15 @@ class NeuralNet(torch.nn.Module):
     def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super().__init__()
         self.linear1 = torch.nn.Linear(input_size, hidden_size)
-        self.linear2 = torch.nn.Linear(hidden_size, output_size)
+        self.linear2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.linear3 = torch.nn.Linear(hidden_size, output_size)
 
     def forward(self, x: torch.Tensor):
         x = self.linear1(x)
         x = torch.nn.functional.leaky_relu(x)
         x = self.linear2(x)
+        x = torch.nn.functional.leaky_relu(x)
+        x = self.linear3(x)
         return x
 
     def save(self, path_to_save: str):
@@ -26,8 +29,9 @@ class NeuralNet(torch.nn.Module):
 
 class QLearning:
 
-    def __init__(self, model: NeuralNet, lr: float, gamma: float) -> None:
+    def __init__(self, model: NeuralNet, target_model: NeuralNet, lr: float, gamma: float) -> None:
         self.model = model
+        self.target_model = target_model
         self.lr = lr
         self.gamma = gamma
         self.optimizer = torch.optim.Adam(model.parameters(), self.lr)
@@ -50,11 +54,13 @@ class QLearning:
 
         target: torch.Tensor = prediction.clone()
 
-        Q_new_next_state_max, _ = torch.max(self.model(new_state), dim=1)
+        with torch.no_grad():
+            best_next_actions = self.model(new_state).argmax(dim=1).unsqueeze(1)
+            Q_new_next_state = self.target_model(new_state).gather(1, best_next_actions)
         done_bool = done.bool() if done.dtype == torch.float else done.bool()
         Q_target_values = reward.clone()
-        Q_new_next_state_max = Q_new_next_state_max.reshape((-1, 1))
-        Q_target_values += self.gamma * Q_new_next_state_max*(~done_bool)
+        Q_new_next_state = Q_new_next_state.reshape((-1, 1))
+        Q_target_values += self.gamma * Q_new_next_state*(~done_bool)
         action_indices = action.long().squeeze()
         target[..., action_indices] = Q_target_values
 
@@ -62,4 +68,5 @@ class QLearning:
         loss = self.criterion(target, prediction)
         loss.backward()
 
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=10)
         self.optimizer.step()  # type: ignore
